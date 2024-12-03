b5b3828295f57de3a083ac73b030456ae90e09cb
nangongxiaoxin <bx_tianhuan@163.com>
2024-12-03 23:00:28 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
+@ -1,10 +1,28 @@
+//导入main.css样式
 import '@/assets/main.css'
-import 'animate.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import { createApp } from 'vue'
 import App from '@/App.vue'
 // 导入路由
 import router from '@/router'
 
+
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+
+
+
+
+
+
+
+
+
+
 const app = createApp(App)
 
 // 应用路由
-app.use(router)
-app.mount('#app')
