2159a15f19ae3a4a55b6c57dd7cc9d8475f55813
nangongxiaoxin <bx_tianhuan@163.com>
2024-12-08 03:23:24 +0800

    login validate

--- src/pages/admin/login.vue
+++ src/pages/admin/login.vue
@@ -1,77 +1,170 @@
 <template>
-    <!-- 使用 grid 网格布局，并指定列数为 2，高度占满全屏 -->
-    <div class="grid grid-cols-2 h-screen">
-        <!-- 默认占两列，order 用于指定排列顺序，md 用于适配非移动端（PC 端） -->
-        <div class="col-span-2 order-2 p-10 md:col-span-1 md:order-1 bg-slate-900">
-            <!-- 指定为 flex 布局，并设置为屏幕垂直水平居中，高度为 100% -->
-            <div class="flex justify-center items-center h-full flex-col">
-                <h2 class="font-bold text-4xl mb-7 text-white">Weblog 博客登录</h2>
-                <p class="text-white">一款由 Spring Boot + Mybaits Plus + Vue 3.2 + Vite 4 开发的前后端分离博客。</p>
-                <!-- 指定图片宽度为父级元素的 1/2 -->
-                <img src="@/assets/eatMelon.png" class="w-1/2">
-            </div>
-        </div>
-        <!-- 分栏 -->
-        <div class="col-span-2 order-1 md:col-span-1 md:order-2 bg-white animate__animated animate__bounceInRight animate__fast">
-            <!-- flex-col 用于指定子元素垂直排列 -->
-            <div class="flex justify-center items-center h-full flex-col">
-                <!-- 大标题，设置字体粗细、大小、下边距 -->
-                <h1 class="font-bold text-4xl mb-5">欢迎回来</h1>
-                <!-- 设置 flex 布局，内容垂直水平居中，文字颜色，以及子内容水平方向 x 轴间距 -->
-                <div class="flex items-center justify-center mb-7 text-gray-400 space-x-2">
-                    <!-- 左边横线，高度为 1px, 宽度为 16，背景色设置 -->
-                    <span class="h-[1px] w-16 bg-gray-200"></span>
-                    <span>账号密码登录</span>
-                    <!-- 右边横线 -->
-                    <span class="h-[1px] w-16 bg-gray-200"></span>
-                </div>
-                <!-- 引入 Element Plus 表单组件，移动端设置宽度为 5/6，PC 端设置为 2/5 -->
-                <el-form class="w-5/6 md:w-2/5">
-                    <el-form-item>
-                        <!-- 输入框组件 -->
-                        <el-input size="large" placeholder="请输入用户名" :prefix-icon="User" clearable v-model="form.username"/>
-                    </el-form-item>
-                    <el-form-item>
-                        <!-- 密码框组件 -->
-                        <el-input size="large" type="password" placeholder="请输入用户名" :prefix-icon="Lock" clearable v-model="form.password"/>
-                    </el-form-item>
-                    <el-form-item>
-                        <!-- 登录按钮，宽度设置为 100% -->
-                        <el-button class="w-full" size="large" type="primary" @click="onSubmit">登录</el-button>
-                    </el-form-item>
-                </el-form>
-
-            </div>
-        </div>
+  <!-- 使用 grid 网格布局，并指定列数为 2，高度占满全屏 -->
+  <div class="grid grid-cols-2 h-screen">
+    <!-- 默认占两列，order 用于指定排列顺序，md 用于适配非移动端（PC 端） -->
+    <div class="col-span-2 order-2 p-10 md:col-span-1 md:order-1 bg-slate-900">
+      <!-- 指定为 flex 布局，并设置为屏幕垂直水平居中，高度为 100% -->
+      <div class="flex justify-center items-center h-full flex-col">
+        <h2 class="font-bold text-4xl mb-7 text-white">Weblog 博客登录</h2>
+        <p class="text-white">
+          一款由 Spring Boot + Mybaits Plus + Vue 3.2 + Vite 4
+          开发的前后端分离博客。
+        </p>
+        <!-- 指定图片宽度为父级元素的 1/2 -->
+        <img src="@/assets/eatMelon.png" class="w-1/2" />
+      </div>
     </div>
+    <!-- 分栏 -->
+    <div
+      class="col-span-2 order-1 md:col-span-1 md:order-2 bg-white animate__animated animate__bounceInRight animate__fast"
+    >
+      <!-- flex-col 用于指定子元素垂直排列 -->
+      <div class="flex justify-center items-center h-full flex-col">
+        <!-- 大标题，设置字体粗细、大小、下边距 -->
+        <h1 class="font-bold text-4xl mb-5">欢迎回来</h1>
+        <!-- 设置 flex 布局，内容垂直水平居中，文字颜色，以及子内容水平方向 x 轴间距 -->
+        <div
+          class="flex items-center justify-center mb-7 text-gray-400 space-x-2"
+        >
+          <!-- 左边横线，高度为 1px, 宽度为 16，背景色设置 -->
+          <span class="h-[1px] w-16 bg-gray-200"></span>
+          <span>账号密码登录</span>
+          <!-- 右边横线 -->
+          <span class="h-[1px] w-16 bg-gray-200"></span>
+        </div>
+        <!-- 引入 Element Plus 表单组件，移动端设置宽度为 5/6，PC 端设置为 2/5 -->
+        <el-form
+          class="w-5/6 md:w-2/5"
+          ref="formRef"
+          :rules="rules"
+          :model="form"
+        >
+          <el-form-item prop="username">
+            <!-- 输入框组件 -->
+            <el-input
+              size="large"
+              v-model="form.username"
+              placeholder="请输入用户名"
+              :prefix-icon="User"
+              clearable
+            />
+          </el-form-item>
+          <el-form-item prop="password">
+            <!-- 密码框组件 -->
+            <el-input
+              size="large"
+              type="password"
+              v-model="form.password"
+              placeholder="请输入密码"
+              :prefix-icon="Lock"
+              clearable
+            />
+          </el-form-item>
+          <el-form-item>
+            <!-- 登录按钮，宽度设置为 100% -->
+            <el-button
+              class="w-full tm-2"
+              size="large"
+              type="primary"
+              :loading="loading"
+              @click="onSubmit"
+              >登录</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 // 引入 Element Plus 中的用户、锁图标
-import { User, Lock } from '@element-plus/icons-vue'
-import { login } from '@/api/admin/user';
-import {reactive} from 'vue';
-import { useRouter } from 'vue-router';
+import { User, Lock } from "@element-plus/icons-vue";
+import { login } from "@/api/admin/user";
+import { ref, reactive, onMounted, onBeforeUnmount } from "vue";
+import { useRouter } from "vue-router";
+import { showMessage } from "@/composables/util";
 
 const router = useRouter();
 
 //定义响应式的表单对象
 const form = reactive({
-    username:'slilio',
-    password:'slilio'
-})
+  username: "slilio",
+  password: "",
+});
+
+//表单引用 响应式引用对象
+const formRef = ref(null);
+//登录加载按钮
+const loading = ref(false);
+
+//表单验证规则
+const rules = {
+  username: [
+    {
+      Required: true,
+      message: "用户名不能为空",
+      trigger: "blur",
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "用户名不能为空",
+      trigger: "blur",
+    },
+  ],
+};
 
 //登录
-const onSubmit=()=>{
-    console.log("login...");
-    login(form.username,form.password).then((res)=>{
-        console.log(res);
-        //判断是否成功
-        if(res.data.success == true){
-            //路由调整到后台首页
-            router.push('/admin/index');
-        }
-    })
+const onSubmit = () => {
+  console.log("login...");
+  //验证 form 表单字段
+  formRef.value.validate((valid) => {
+    if (!valid) {
+      console.log("表单验证失败");
+      return false;
+    }
+
+    //开始加载
+    loading.value=true;
+
+  });
+
+  //调用接口登录
+  login(form.username, form.password).then((res) => {
+    console.log(res);
+    //判断是否成功
+    if (res.data.success == true) {
+      showMessage("登录成功");
+      //路由调整到后台首页
+      router.push("/admin/index");
+    } else {
+      let message = res.data.message;
+      showMessage(message, "error");
+    }
+  }).finally(()=>{
+    //结束加载
+    loading.value = false;
+  });
+};
+
+//键盘友好支持
+function onkeyup(e) {
+  console.log(e);
+  if (e.key == "Enter") {
+    onSubmit();
+  }
 }
 
+//添加键盘监听
+onMounted(() => {
+  console.log("添加键盘监听");
+  document.addEventListener("keyup", onkeyup);
+});
+
+//移除键盘监听
+onBeforeUnmount(() => {
+  document.removeEventListener("keyup", onkeyup);
+});
 </script>
